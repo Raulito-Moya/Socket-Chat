6d0fd778f62bf2d594710eeda99b9f6a0f6073d9
Raulito-Moya <rauly7moya@gmail.com>
2021-05-31 17:41:26 -0500

    Chat finalizado

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,26 +1,27 @@
 var socket = io();
 
-var params = new URLSearchParams( window.location.search )
+var params = new URLSearchParams(window.location.search);
 
-if ( !params.has('nombre') || !params.has('sala') ) {
+if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
-    throw new Error('El nombre y sala son necesarios')
+    throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-}
+};
+
+
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
-    socket.emit('entrarChat', usuario, function( resp ){
-       
-        console.log('Usuarios Conectados',resp);
-    })
+    socket.emit('entrarChat', usuario, function(resp) {
+       // console.log('Usuarios conectados', resp);
+        renderizarUsuario( resp );
+    });
 
-    
 });
 
 // escuchar
@@ -31,32 +32,25 @@ socket.on('disconnect', function() {
 });
 
 
-// // Enviar información
-// socket.emit('enviarMensaje', {
-//     usuario: 'Fernando',
-//     mensaje: 'Hola Mundo'
-// }, function(resp) {
-//     console.log('respuesta server: ', resp);
-// });
+
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
     console.log('Servidor:', mensaje);
-
+    renderizarMensajes(mensaje, false)
+     scrollBottom() 
 });
 
-
-//escuchar cambios de usuario 
-//cuando un usuario entra o sale del chat
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-
-    console.log(personas);
-
+  
+    renderizarUsuario( personas );
 });
 
+// Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
-   
-  console.log('Mensaje Privado:', mensaje);
 
-})
+    console.log('Mensaje Privado:', mensaje);
+
+});
